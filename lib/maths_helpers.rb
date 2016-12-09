10ec2fbaa7cdf0aff6ad1382ed2d5c2e6696acf6
itggot-oliver-alnas <oliver.alnas@itggot.se>
2016-12-09 12:28:43 +0100

    Funktioner
    Alla funktioner förutom square.

--- lib/maths_helpers.rb
+++ lib/maths_helpers.rb
@@ -1,1 +1,23 @@
+def halve(value: )
+    value /= 2
+    return value
+end
+
+def add(term1: , term2: )
+    sum = 0
+    sum = sum + term1 + term2
+    return sum
+end
+
+def subtract(term1: , term2: )
+    sum = 0
+    sum = term1/term2
+    return sum
+end
+
+def multiply(factor1: , factor2: )
+    sum = 0
+    sum = factor1*factor2
+    return sum
+end
 
